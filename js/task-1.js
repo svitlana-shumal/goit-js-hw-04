c246f2731dd2b7e9de6fb63c8c2e30c93030b72d
Svitlana Shumal <svetlankachajka@gmail.com>
2025-04-23 10:23:48 +0300

    added code

--- js/task-1.js
+++ js/task-1.js
@@ -1,16 +1,16 @@
-// function isEnoughCapacity(products, containerSize) {
-//   let totalProducts = 0;
-//   const values = Object.values(products);
-//   for (const prod of values) {
-//     totalProducts += prod;
-//   }
-//   if (totalProducts <= containerSize) {
-//     return true;
-//   }
-//   return false;
-// }
+function isEnoughCapacity(products, containerSize) {
+  let totalProducts = 0;
+  const values = Object.values(products);
+  for (const prod of values) {
+    totalProducts += prod;
+  }
+  if (totalProducts <= containerSize) {
+    return true;
+  }
+  return false;
+}
 
-// console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
-// console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
-// console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
-// console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8));
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12));
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14));
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7));
--- js/task-3.js
+++ js/task-3.js
@@ -1,34 +1,21 @@
-// const profile = {
-//     username: "Jacob",
-//     playTime: 300,
-// },
-// getInfo: ();
-// changeUsername(newName)
-//   for (const name of this.profile) {
-//     if (name.username === profile.username) {
-//       name.username = newName;
-//     }
-// }
-// console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+const profile = {
+  username: "Jacob",
+  playTime: 300,
+  changeUsername(newName) {
+    this.username = newName;
+  },
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+};
 
-// profile.changeUsername("Marco");
-// console.log(profile.getInfo()); // "Marco has 300 active hours!"
+console.log(profile.getInfo());
 
-// profile.updatePlayTime(20);
-// console.log(profile.getInfo()); // "Marco has 320 active hours!"
-// // Доповни об’єкт profile методами для роботи з його властивостями.
+profile.changeUsername("Marco");
+console.log(profile.getInfo());
 
-// Метод changeUsername(newName) повинен приймати рядок (нове ім’я) в параметр newName та змінювати значення властивості username на нове. Нічого не повертає.
-// Метод updatePlayTime(hours) повинен приймати число (кількість годин) у параметр hours та збільшити на нього значення властивості playTime. Нічого не повертає.
-// Метод getInfo() має повертати рядок формату <Username> has <amount> active hours!, де <Username> — це ім’я профілю, а <amount> — кількість ігрових годин.
-// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
-
-// Залиш цей код для перевірки ментором.
-
-// На що буде звертати увагу ментор при перевірці:
-// Оголошена змінна profile
-// Значення змінної profile — це об’єкт з властивостями username, playTime, getInfo, changeUsername і updatePlayTime
-// Значення властивості getInfo — це функція
-// Значення властивості changeUsername — це функція
-// Значення властивості updatePlayTime — це функція
-// Для звернення до властивостей об’єкта в його методах використовується this
+profile.updatePlayTime(20);
+console.log(profile.getInfo());
